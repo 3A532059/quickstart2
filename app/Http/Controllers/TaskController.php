28eb06f5889c67ff25d40676dcbe7c1b35e8ea66
3A532059 <coco214034@gmail.com>
2018-11-20 19:29:22 +0800

    定義TaskController的store方法，使其可存放新增的task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,4 +17,35 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
+
+    /**
+     * 顯示使用者所有任務的清單。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+
+    /**
+     * 建立新的任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
 }
